63f9580a353b54b2e407ab69e9c9650d88a24fdc
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2018-11-15 14:40:16 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,6 @@ import {reducer} from "./components/reducers";
 import {getTeamPlayers, getTeam} from "./components/actions";
 
 class App extends Component {
-<<<<<<< HEAD
-=======
-  render() {
-      const store = createStore(reducer)
-
-
-      store.subscribe(() => {
-          console.log("Store changed ", store.getState())
-      })
-
-      store.dispatch(getTeamPlayers(344))
-      store.dispatch(getTeamPlayers(344))
-      store.dispatch(getTeam(16))
-
-
-
-
-
->>>>>>> bcf539801df411f6bf1aa2e1354ec87e02b0470f
 
     componentWillMount() {
         const store = createStore(reducer)
