42398290e6b65081ec7fa9b1204447853770dcf5
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2018-11-15 14:12:32 +0000

    Update actions.js

--- src/components/actions.js
+++ src/components/actions.js
@@ -7,10 +7,10 @@ export const CONSTANTS =  {
 
 
 
-export const getTeam = () => {
+export const getTeam = (team) => {
         return {
         type: CONSTANTS.REQUEST_TEAMS,
-        dispatch : axios.get("https://api.football-data.org//v2/teams/2", {
+        dispatch : axios.get("https://api.football-data.org//v2/teams/" + team +"", {
             headers: {
                 'X-Auth-Token' : '5574cdbc43b04887b4f8bd52329d0c3f'
             }})
